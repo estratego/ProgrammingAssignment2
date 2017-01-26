77280646d81b39f5dff7148aba1ac3d5b8984acb
estratego <rafaeldeheredia@hotmail.com>
2017-01-26 12:28:37 +0100

    Add files via upload
    Here is my solution for the Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function will first set the matrix, solve it's inverse and cache the result.
+## Then 
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() {x}
+        setinverse <- function(solve) {m <<- solve}
+        getinverse <- function() {m}
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve will check if a value is already attributed to m (and then just recover it without
+## losing computational time) or calculate it in case it doesn't exist.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
